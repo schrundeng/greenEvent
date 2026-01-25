078e87f7fc39e2e821c735f3e188028810fcba91
schrundeng <yangticamilan@gmail.com>
2026-01-25 10:41:36 +0700

    added build for render

---
+++ build.sh
@@ -0,0 +1,7 @@
+composer install --no-dev --optimize-autoloader
+php artisan config:cache
+php artisan route:cache
+php artisan view:cachecomposer install --no-dev --optimize-autoloader
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
